cade8be451bae23547a9d1123cc55b23d02ec435
Fikscriper <143531648+Fikscriper@users.noreply.github.com>
2025-10-01 18:45:12 +0300

    fixed

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const scrollButton = document.querySelector(".scroll-top");
+
+scrollButton.addEventListener
+(
+    "click", () => 
+    {
+       window.scrollTo({
+        top:0,
+        behavior: "smooth"
+       })
+    },
+    false,
+);
